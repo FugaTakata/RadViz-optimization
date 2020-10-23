590b7281def7c3e7ece546acd3965d4a48e3500a
FugaTakata <maple1213goo@gmail.com>
2020-10-23 16:31:40 +0900

    update

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import RadViz from "./RadViz";
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [dimensions, setDimensions] = useState([]);
+
+  useEffect(() => {
+    const dataURL = "data/iris.json";
+    fetch(dataURL)
+      .then((res) => res.json())
+      .then((data) => {
+        const max = {};
+        const min = {};
+        const keys = Object.keys(data[0]);
+        setDimensions(keys.filter((key) => key != "species"));
+        keys.map((key) => {
+          max[key] = data[0][key];
+          min[key] = data[0][key];
+        });
+        data.map((item) => {
+          keys.map((key) => {
+            if (max[key] < item[key]) {
+              max[key] = item[key];
+            }
+            if (item[key] < min[key]) {
+              min[key] = item[key];
+            }
+          });
+        });
+        data.map((item) => {
+          keys.map((key) => {
+            item[key] = (item[key] - min[key]) / (max[key] - min[key]);
+          });
+        });
+        setData(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <RadViz data={data} dimensions={dimensions} colorKey={"species"} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/RadViz.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from "react";
+import * as d3 from "d3";
+
+const optimizer = (data, dimensions, i) => {
+  const d = [];
+  if (data.length > 0) {
+    dimensions
+      .map((property) => [property, data[i][property]])
+      .sort((a, b) => b[1] - a[1])
+      .map((item, i) => {
+        if (i % 2 == 0) {
+          d.push(item[0]);
+        } else {
+          d.unshift(item[0]);
+        }
+      });
+  }
+  return d;
+};
+
+const radviz = (data, dimensions, r) => {
+  const n = dimensions.length;
+  const scales = dimensions.map((property) => {
+    return d3
+      .scaleLinear()
+      .domain(d3.extent(data, (item) => item[property]))
+      .range([0, 1]);
+  });
+  return data.map((item, i) => {
+    let a = 0;
+    let b = 0;
+    let c = 0;
+    const dt = (2 * Math.PI) / n;
+    for (let j = 0; j < n; ++j) {
+      const v = scales[j](item[dimensions[j]]);
+      a += v * Math.cos(dt * j);
+      b += v * Math.sin(dt * j);
+      c += v;
+    }
+    a *= r / c;
+    b *= r / c;
+    const d = Math.sqrt(a * a + b * b);
+    const t = Math.atan2(b, a);
+    return { x: d * Math.cos(t), y: d * Math.sin(t) };
+  });
+};
+
+const RadViz = ({ data, dimensions, colorKey }) => {
+  const r = 300;
+  const contentWidth = 2 * r;
+  const contentHeight = 2 * r;
+  const margin = 50;
+  const width = contentWidth + margin * 2;
+  const height = contentHeight + margin * 2;
+  const lineColor = "#444";
+
+  const color = d3.scaleOrdinal(d3.schemeCategory10);
+  for (const item of data) {
+    color(item[colorKey]);
+  }
+
+  dimensions = optimizer(data, dimensions, 0);
+
+  const points = radviz(data, dimensions, r);
+
+  return (
+    <svg viewBox={`0 0 ${width} ${height}`}>
+      <g transform={`translate(${margin + r},${margin + r})`}>
+        <circle r={r} fill="none" stroke={lineColor} />
+        {dimensions.map((property, i) => {
+          return (
+            <g
+              key={i}
+              transform={`rotate(${(360 / dimensions.length) * i + 90})`}
+            >
+              <line x1="0" y1="0" x2="0" y2={-r} stroke={lineColor} />
+              <text
+                y={-r}
+                textAnchor="middle"
+                dominantBaseline="text-after-edge"
+              >
+                {property}
+              </text>
+            </g>
+          );
+        })}
+        {data.map((item, i) => {
+          const { x, y } = points[i];
+          return (
+            <g key={i} transform={`translate(${x},${y})`}>
+              <circle r="3" fill={color(item[colorKey])} opacity="0.8">
+                <title>{dimensions.map((p) => item[p]).join(",")}</title>
+              </circle>
+            </g>
+          );
+        })}
+      </g>
+    </svg>
+  );
+};
+
+export default RadViz;
